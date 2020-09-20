2c9dddf9b18b5249a946949f040d4c54ed579a32
Marin Hyatt <mhyatt7@gatech.edu>
2020-09-20 19:09:43 +0000

    Completed project 1.3

--- project_1_3/project_1_3.cpp
+++ project_1_3/project_1_3.cpp
@@ -36,20 +36,22 @@ int main() {
   w = readInVector(s);
   // TODO write your code here
   // =========== START =========
-  if (pack_with_zeros) {
-    //pack with zeros!
-
-    //first, calculate how many zeroes to add at beginning and end
+  //first, calculate how many zeroes or other numbers to add at beginning and end
     int num_zeros = (w.size() - 1) / 2;
 
-    //create adjusted vector with space for zero padding at beginning and end
+    //create adjusted vector with space for padding at beginning and end
     std::vector<double> x_adjusted(x.size() + 2 * num_zeros);
     std::cout << x.size() + 2 <<std::endl;
 
-    //set first and last elements to 0
+    //set first and last elements to either 0 or closest element of x
     for (int count = 0; count < num_zeros; count++) {
-      x_adjusted[count] = 0;
-      x_adjusted[x_adjusted.size() - (count + 1)]= 0;
+      if (pack_with_zeros) {
+        x_adjusted[count] = 0;
+        x_adjusted[x_adjusted.size() - (count + 1)]= 0;
+      } else {
+        x_adjusted[count] = x[0];
+        x_adjusted[x_adjusted.size() - (count + 1)]= x[x.size() - 1];
+      }
     }
 
     //deep copy of x goes in the middle 
@@ -67,46 +69,36 @@ int main() {
       y.push_back(y_element);
     }
 
-    //print y
-    for (double i : y) {
-      std::cout << i << " ";
-    }
-  } else {
-    //use closest array index instead!
-    //first, calculate how many numbers to add at beginning and end
-    int num_non_zeros = (w.size() - 1) / 2;
-
-    //create adjusted vector with space for zero padding at beginning and end
-    std::vector<double> x_adjusted(x.size() + 2 * num_non_zeros);
-    std::cout << x.size() + 2 <<std::endl;
-
-    //set first and last elements to 0
-    for (int count = 0; count < num_non_zeros; count++) {
-      x_adjusted[count] = x[0];
-      x_adjusted[x_adjusted.size() - (count + 1)]= x[x.size() - 1];
-    }
-
-    //deep copy of x goes in the middle 
-    for (int i = 0; i < x.size(); i++) {
-      x_adjusted[i + num_non_zeros] = x[i];
-    }
-
-    //start filling in elements of y
-    for (int count = 0; count < x.size(); count++) {
-      //for each element in the kernel:
-      double y_element = 0;
-      for (int j = 0; j < w.size(); j++) {
-        y_element += (x_adjusted[j + count] * w[j]);
+    //print x
+    std::cout << "x: {";
+    for (double i = 0; i < x.size(); i++) {
+      std::cout << x[i];
+      if (i < x.size() - 1) {
+        std::cout << ", ";
       }
-      y.push_back(y_element);
     }
+    std::cout << "}" << std::endl;
+
+    //print w
+    std::cout << "w: {";
+    for (double i = 0; i < w.size(); i++) {
+      std::cout << w[i];
+      if (i < w.size() - 1) {
+        std::cout << ", ";
+      }
+    }
+    std::cout << "}" << std::endl;
 
     //print y
-    for (double i : y) {
-      std::cout << i << " ";
+    std::cout << "y: {";
+    for (double i = 0; i < y.size(); i++) {
+      std::cout << y[i];
+      if (i < y.size() - 1) {
+        std::cout << ", ";
+      }
     }
-  }
-  
+    std::cout << "}" << std::endl;
+
   // =========== END ===========
 
   return 0;
