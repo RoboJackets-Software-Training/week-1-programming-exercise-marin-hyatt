7852e4dd93fbb196beb49a4950ca17ac5b6daac0
Marin Hyatt <mhyatt7@gatech.edu>
2020-09-20 19:00:32 +0000

    Implemented part 2 of project 1.3

--- project_1_3/project_1_3.cpp
+++ project_1_3/project_1_3.cpp
@@ -36,59 +36,77 @@ int main() {
   w = readInVector(s);
   // TODO write your code here
   // =========== START =========
-  //pack with zeros!
+  if (pack_with_zeros) {
+    //pack with zeros!
 
-  //first, calculate how many zeroes to add at beginning and end
-  int num_zeros = (w.size() - 1) / 2;
+    //first, calculate how many zeroes to add at beginning and end
+    int num_zeros = (w.size() - 1) / 2;
 
-  //create adjusted vector with space for zero padding at beginning and end
-  std::vector<double> x_adjusted(x.size() + 2 * num_zeros);
-  std::cout << x.size() + 2 <<std::endl;
+    //create adjusted vector with space for zero padding at beginning and end
+    std::vector<double> x_adjusted(x.size() + 2 * num_zeros);
+    std::cout << x.size() + 2 <<std::endl;
 
-  //set first and last elements to 0
-  for (int count = 0; count < num_zeros; count++) {
-    x_adjusted[count] = 0;
-    x_adjusted[x_adjusted.size() - (count + 1)]= 0;
-  }
+    //set first and last elements to 0
+    for (int count = 0; count < num_zeros; count++) {
+      x_adjusted[count] = 0;
+      x_adjusted[x_adjusted.size() - (count + 1)]= 0;
+    }
 
-  //test print x_adjusted
-  std::cout << "x_adjusted: ";
-  for (double i : x_adjusted) {
-    std::cout << i << " ";
-  }
-  std::cout << "x_adjusted size: " << x_adjusted.size() <<std::endl;
+    //deep copy of x goes in the middle 
+    for (int i = 0; i < x.size(); i++) {
+      x_adjusted[i + num_zeros] = x[i];
+    }
 
-  //deep copy of x goes in the middle 
-  for (int i = 0; i < x.size(); i++) {
-    x_adjusted[i + num_zeros] = x[i];
+    //start filling in elements of y
+    for (int count = 0; count < x.size(); count++) {
+      //for each element in the kernel:
+      double y_element = 0;
+      for (int j = 0; j < w.size(); j++) {
+        y_element += (x_adjusted[j + count] * w[j]);
+      }
+      y.push_back(y_element);
+    }
+
+    //print y
+    for (double i : y) {
+      std::cout << i << " ";
+    }
+  } else {
+    //use closest array index instead!
+    //first, calculate how many numbers to add at beginning and end
+    int num_non_zeros = (w.size() - 1) / 2;
+
+    //create adjusted vector with space for zero padding at beginning and end
+    std::vector<double> x_adjusted(x.size() + 2 * num_non_zeros);
+    std::cout << x.size() + 2 <<std::endl;
+
+    //set first and last elements to 0
+    for (int count = 0; count < num_non_zeros; count++) {
+      x_adjusted[count] = x[0];
+      x_adjusted[x_adjusted.size() - (count + 1)]= x[x.size() - 1];
+    }
+
+    //deep copy of x goes in the middle 
+    for (int i = 0; i < x.size(); i++) {
+      x_adjusted[i + num_non_zeros] = x[i];
+    }
+
+    //start filling in elements of y
+    for (int count = 0; count < x.size(); count++) {
+      //for each element in the kernel:
+      double y_element = 0;
+      for (int j = 0; j < w.size(); j++) {
+        y_element += (x_adjusted[j + count] * w[j]);
+      }
+      y.push_back(y_element);
+    }
+
+    //print y
+    for (double i : y) {
+      std::cout << i << " ";
+    }
   }
   
-  //test print x_adjusted
-  std::cout << "x_adjusted: ";
-  for (double i : x_adjusted) {
-    std::cout << i << " ";
-  }
-  std::cout << "x_adjusted size: " << x_adjusted.size() <<std::endl;
-
-  // //start filling in elements of y
-  // for (int i = num_zeros; i < x.size() + num_zeros; i++) {
-  //   y.push_back(x_adjusted[i-1] * w[0] + x_adjusted[i] * w[1] + x_adjusted[i+1] * w[2]);
-  // }
-
-  //start filling in elements of y
-  for (int count = 0; count < x.size(); count++) {
-    //for each element in the kernel:
-    double y_element = 0;
-    for (int j = 0; j < w.size(); j++) {
-      y_element += (x_adjusted[j + count] * w[j]);
-    }
-    y.push_back(y_element);
-  }
-
-  //print y
-  for (double i : y) {
-    std::cout << i << " ";
-  }
   // =========== END ===========
 
   return 0;
