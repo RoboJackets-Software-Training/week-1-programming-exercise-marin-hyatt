89cedda207dd1b659c39d2eab7259cb2762b500f
Marin Hyatt <mhyatt7@gatech.edu>
2020-09-20 18:39:02 +0000

    Completed part 1 of implementation of project 1.3

--- project_1_3/project_1_3.cpp
+++ project_1_3/project_1_3.cpp
@@ -34,13 +34,61 @@ int main() {
   x = readInVector(s);
   std::cin >> s;
   w = readInVector(s);
-
   // TODO write your code here
   // =========== START =========
+  //pack with zeros!
 
+  //first, calculate how many zeroes to add at beginning and end
+  int num_zeros = (w.size() - 1) / 2;
 
+  //create adjusted vector with space for zero padding at beginning and end
+  std::vector<double> x_adjusted(x.size() + 2 * num_zeros);
+  std::cout << x.size() + 2 <<std::endl;
 
+  //set first and last elements to 0
+  for (int count = 0; count < num_zeros; count++) {
+    x_adjusted[count] = 0;
+    x_adjusted[x_adjusted.size() - (count + 1)]= 0;
+  }
 
+  //test print x_adjusted
+  std::cout << "x_adjusted: ";
+  for (double i : x_adjusted) {
+    std::cout << i << " ";
+  }
+  std::cout << "x_adjusted size: " << x_adjusted.size() <<std::endl;
+
+  //deep copy of x goes in the middle 
+  for (int i = 0; i < x.size(); i++) {
+    x_adjusted[i + num_zeros] = x[i];
+  }
+  
+  //test print x_adjusted
+  std::cout << "x_adjusted: ";
+  for (double i : x_adjusted) {
+    std::cout << i << " ";
+  }
+  std::cout << "x_adjusted size: " << x_adjusted.size() <<std::endl;
+
+  // //start filling in elements of y
+  // for (int i = num_zeros; i < x.size() + num_zeros; i++) {
+  //   y.push_back(x_adjusted[i-1] * w[0] + x_adjusted[i] * w[1] + x_adjusted[i+1] * w[2]);
+  // }
+
+  //start filling in elements of y
+  for (int count = 0; count < x.size(); count++) {
+    //for each element in the kernel:
+    double y_element = 0;
+    for (int j = 0; j < w.size(); j++) {
+      y_element += (x_adjusted[j + count] * w[j]);
+    }
+    y.push_back(y_element);
+  }
+
+  //print y
+  for (double i : y) {
+    std::cout << i << " ";
+  }
   // =========== END ===========
 
   return 0;
